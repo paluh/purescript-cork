56409fbef631c3beec5353daf7587eda75fe0bbc
Tomasz Rybarczyk <paluho@gmail.com>
2020-11-04 09:23:55 +0100

    Let's set the dimensions of the svg

--- src/Cork/Svg.purs
+++ src/Cork/Svg.purs
@@ -2,11 +2,9 @@ module Cork.Svg
   ( module Types
   , render
   , render'
-  )
-  where
+  ) where
 
 import Prelude
-
 import Cork.Svg.Attributes (viewBox) as Attributes
 import Cork.Svg.Types (Document(..), Render(..))
 import Cork.Svg.Types (Document(..), Render(..)) as Types
@@ -30,31 +28,34 @@ xmlns = M.attribute "xmlns"
 render ∷ ∀ e. Document e → Maybe (ConversionFactor Units.Scene Units.Pixel) → Render
 render (Document { body, defs, viewBox: viewBox@(BoundingBox viewBoxV) }) mRatio = Render $ document
   where
-    document = Smolder.render $ M.svg
+  document =
+    Smolder.render $ M.svg
       ! A.viewBox (Attributes.viewBox viewBox)
       ! A.overflow "hidden"
       -- ! A.style "width:100%;height:100%"
-      -- ! case mRatio of
-      --     -- | When we use 100% as width value it seems that there is no clipping.
-      --     -- | 
-      --     -- | I'm not sure if we want to handle this in such an unresponsive way...
-      --     Nothing →
-      --       A.width ("100%")
-      --       <> A.height ("auto")
-      --       -- A.height (show $ Distance.toNumber viewBoxV.height)
-      --       -- <> A.width (show $ Distance.toNumber viewBoxV.width)
-      --     Just ratio →
-      --       let
-      --         Distance height = Distance.convert ratio viewBoxV.height
-      --         Distance width = Distance.convert ratio viewBoxV.width
-      --       in
-      --         A.height (show height) <> A.width (show width)
+      
+      ! case mRatio of
+          -- | When we use 100% as width value it seems that there is no clipping.
+          -- | 
+          -- | I'm not sure if we want to handle this in such an unresponsive way...
+          Nothing → mempty
+          -- A.width ("100%")
+          -- <> A.height ("auto")
+          -- -- A.height (show $ Distance.toNumber viewBoxV.height)
+          -- -- <> A.width (show $ Distance.toNumber viewBoxV.width)
+          Just ratio →
+            let
+              Distance height = Distance.convert ratio viewBoxV.height
+
+              Distance width = Distance.convert ratio viewBoxV.width
+            in
+              A.height (show height) <> A.width (show width)
       ! A.baseProfile "full"
       ! xmlns "http://www.w3.org/2000/svg"
       ! A.version "1.1"
       $ do
-        M.defs defs
-        body
+          M.defs defs
+          body
 
 render' ∷ ∀ e. Document e → Render
 render' d = render d Nothing
